50b687c8434417df06dbc9b4d66d8adf1e4af3cc
Haider Mushtaq (ext.) <hmhaider93@gmail.com>
2025-03-24 22:32:21 +0100

    Adding rainbow buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,79 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            color: Colors.blueGrey,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.lightGreen
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.purple
+                  ),
+                  child: Container(
+                    child: Text(""),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
