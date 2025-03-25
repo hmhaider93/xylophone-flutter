f3353a3721a2859c10c0fc53d37ca645051e1507
Haider Mushtaq (ext.) <hmhaider93@gmail.com>
2025-03-25 12:49:50 +0100

    Adding sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+// import 'package:just_audio/just_audio.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  Future<void> playSound(int soundNumber) async {
+    final player1 = AudioPlayer(); 
+    await player1.setSource(AssetSource('note$soundNumber.wav'));
+    player1.resume();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,34 +25,31 @@ class XylophoneApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Expanded(
-                  child: TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero)),
+                  child: GestureDetector(
+                    onTap: () async {
+                      playSound(1);
+                    },
                     child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () async{
+                      playSound(2);
+                    },
+                    child: Container(
+                      color: Colors.orange,
                       child: SizedBox.expand(),
                     ),
                   ),
                 ),
                 Expanded(
                   child: TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero)),
-                    child: Container(
-                      child: SizedBox.expand(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      playSound(3);
+                    },
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.yellow,
                         padding: EdgeInsets.zero,
@@ -56,7 +62,9 @@ class XylophoneApp extends StatelessWidget {
                 ),
                 Expanded(
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      playSound(4);
+                    },
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.lightGreen,
                         padding: EdgeInsets.zero,
@@ -69,7 +77,9 @@ class XylophoneApp extends StatelessWidget {
                 ),
                 Expanded(
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      playSound(5);
+                    },
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.green,
                         padding: EdgeInsets.zero,
@@ -82,7 +92,9 @@ class XylophoneApp extends StatelessWidget {
                 ),
                 Expanded(
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      playSound(6);
+                    },
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.blue,
                         padding: EdgeInsets.zero,
@@ -95,7 +107,9 @@ class XylophoneApp extends StatelessWidget {
                 ),
                 Expanded(
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      playSound(7);
+                    },
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.purple,
                         padding: EdgeInsets.zero,
